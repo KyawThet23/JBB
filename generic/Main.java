7c32e12d764df66986d7047e4a9ae989aaf3fd34
kyawThetHtun <kyawthe235@gmail.com>
2024-10-12 08:35:01 +0630

    solution with generic

--- generic/Main.java
+++ generic/Main.java
@@ -3,7 +3,12 @@ package generic;
 public class Main {
 
     public static void main(String[] args) {
-        var list = new List();
-        list.add(1);
+        var list1 = new GenericList<Integer>();
+        list1.add(1);
+        int number = list1.get(0);
+        var list2 = new GenericList<User>();
+        list2.add(new User());
+        User user = list2.get(0);
+
     }
 }
