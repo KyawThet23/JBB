243a7c7d30322fe76bced51ccd141f39a0f01d7f
KyawThet23 <kyawthe235@gmail.com>
2024-10-17 18:13:06 +0630

    collection implementation

--- collections/Main.java
+++ collections/Main.java
@@ -1,8 +1,22 @@
 package collections;
 
+import generic.GenericList;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        var list = new GenericList<String>();
+        var iterator = list.iterator();
+        while (iterator.hasNext()) {
+            var current = iterator.next();
+            System.out.println(current);
+        }
+
+        // you can write above as like this => byte code still implements hasNext method.
+        for (String current : list) {
+            System.out.println(current);
+        }
 
     }
 }
