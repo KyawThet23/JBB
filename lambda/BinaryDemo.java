eaf9b6285d9408f22f39d07e2c80b2b68ddc3ada
kyawThetHtun <kyawthe235@gmail.com>
2024-11-15 23:35:08 +0630

    for github

--- lambda/BinaryDemo.java
+++ lambda/BinaryDemo.java
@@ -7,6 +7,8 @@ public class BinaryDemo {
 
     }
 
+    //nothing , I just want to add github commit history
+
     public static void show(){
         BinaryOperator<Integer> add = (a,b) -> a+b;
         var result = add.apply(1,2);
